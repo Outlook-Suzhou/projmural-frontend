4911244869b74a9ef3b683c5cd71d92bdf9fa951
Renxin Wang <t-renxinwang@microsoft.com>
2021-07-21 15:24:41 +0800

    add toolbar

---
+++ src/components/tool_bar/tool_bar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './tool_bar.scss';
+
+const ToolBar: React.FC<{}> = () => {
+  const a = 1;
+  return (
+    <div className="toolbar">
+      {a}
+    </div>
+  );
+};
+export default ToolBar;
---
+++ src/pages/painting/painting.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToolBar from '../../components/tool_bar/tool_bar';
+
+const Paiting: React.FC<{}> = () => {
+  const a = 1;
+  return (
+    <div className="paiting">
+      {a}
+      <ToolBar />
+    </div>
+  );
+};
+export default Paiting;
