994da7a2870365004fe177dc08c35fdc40f73443
Renxin Wang <t-renxinwang@microsoft.com>
2021-07-23 14:18:48 +0800

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Painting from './pages/painting/painting';
 
-const App: React.FC<{}> = () => {
-  const a = 1;
-  return (
-    <div>
-      {a}
-      <Painting />
-    </div>
-  );
-};
+const App: React.FC<{}> = () => (
+  <Router>
+    <Switch>
+      <Route path="/login">
+        login
+      </Route>
+      <Route path="/dashboard">
+        dashboard
+      </Route>
+      <Route path="/painting">
+        <Painting />
+      </Route>
+      <Route path="/">
+        login
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
