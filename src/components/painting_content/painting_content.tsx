bd6a948f60ad998190ba8d4d71bacf72866e3731
lyxxn0414 <181250077@smail.nju.edu.cn>
2021-07-26 20:52:32 +0800

    add text

--- src/components/painting_content/painting_content.tsx
+++ src/components/painting_content/painting_content.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect, useState } from 'react';
 import { Stage, Layer } from 'react-konva';
+import { Modal } from 'antd';
 import doc from '../../client/client';
 import Ellipse from '../shapes/ellipse';
 import Rectangle from '../shapes/rectangle';
@@ -11,11 +12,25 @@ import AddShape from '../tool_bar/tools/add_shape';
 import ToolBar from '../tool_bar/tool_bar';
 import Img from '../shapes/image';
 import AddImage from '../tool_bar/tools/add_images';
+import Text from '../shapes/text';
+import AddText from '../tool_bar/tools/add_text';
 
 const PaitingContent: React.FC<{}> = () => {
   const [list, setList] = useState(doc?.data?.shapes || []);
   const [currentItem, setCurrentItem] = useState({});
   const [currentIndex, setCurrentIndex] = useState(-1);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  // const showModal = () => {
+  //   setIsModalVisible(true);
+  // };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   useEffect(() => {
     doc.subscribe(() => {
       if (doc?.data?.shapes) {
@@ -33,7 +48,7 @@ const PaitingContent: React.FC<{}> = () => {
   return (
     <div>
       <div>
-        <ToolBar width={80} height={200} list={[AddShape, AddImage]} currentShape={currentItem} currentIndex={currentIndex} />
+        <ToolBar width={80} height={200} list={[AddShape, AddImage, AddText]} currentShape={currentItem} currentIndex={currentIndex} />
       </div>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
@@ -67,11 +82,19 @@ const PaitingContent: React.FC<{}> = () => {
                   return (
                     <Img item={item} index={index} click={() => { setCurrentItem(item); setCurrentIndex(index); console.log(item); }} />
                   );
+                case 'TEXT':
+                  // eslint-disable-next-line consistent-return
+                  return (
+                    <Text item={item} index={index} click={() => { setCurrentItem(item); setCurrentIndex(index); console.log(item); }} />
+                  );
               }
             })
           }
         </Layer>
       </Stage>
+      <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        abcd
+      </Modal>
     </div>
   );
 };
--- src/pages/painting/painting.tsx
+++ src/pages/painting/painting.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, {useState} from 'react';
 import PaitingContent from '../../components/painting_content/painting_content';
 import ToolBar from '../../components/tool_bar/tool_bar';
 import AddShape from '../../components/tool_bar/tools/add_shape';
@@ -7,14 +7,30 @@ import addRectangle from '../../utils/add_rectangle';
 import addCircle from '../../utils/add_circle';
 import addEllipse from '../../utils/add_circle';
 import addDiamond from '../../utils/add_diamond';
+import {Modal} from "antd";
 
 const Paiting: React.FC<{}> = () => {
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal=()=>{
+        setIsModalVisible(true);
+    }
   return (
     <div className="paiting" >
       <button type="button" onClick={() => addRectangle({width: 20, height: 50, x: 30, y: 40, type: 'RECTANGLE'})}>rect</button>
       <button type="button" onClick={() => addCircle({radius: 30, x: 30, y: 40, type: 'CIRCLE'})}>circle</button>
       <button type="button" onClick={() => addEllipse({radius: {x: 20, y: 30}, x: 30, y: 40, type: 'ELLIPSE'})}>ellipse</button>
       <button type="button" onClick={() => addDiamond({radius: {x: 40, y: 25}, x: 30, y: 40, type: 'DIAMOND'})}>diamond</button>
+        <Modal visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+            abcd
+        </Modal>
       <PaitingContent />
     </div>
   );
