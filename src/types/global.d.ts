9a176c5d0234aa2b6c4cbe4a149d7546d47a79d6
ZerQAQ <1075843852@qq.com>
2021-07-26 16:20:02 +0800

    add circle ellipse diamond interface

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,10 +1,25 @@
+import Ellipse from "../components/shapes/ellipse";
+
 /* eslint-disable no-unused-vars */
 declare namespace BaseShapes {
-  interface Rectangle {
-    width: number,
-    height: number,
+  interface Position {
     x: number,
     y: number,
+  }
+  interface Rectangle extends Position {
+    width: number,
+    height: number,
     type: 'RECTANGLE'
+  }
+  interface Circle extends Position {
+    radius: number,
+    type: 'CIRCLE'
+  }
+  interface Ellipse extends Position {
+    radius: Position,
+    type: "ELLIPSE"
+  }
+  interface Diamond extends Ellipse {
+    Ellipse,
   }
 }
