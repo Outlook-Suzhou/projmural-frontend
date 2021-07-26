44d9419a80e9157373f79abb117d71826b30578a
inctry <inctry@outlook.com>
2021-07-26 14:38:40 +0800

    add global types

---
+++ src/types/index.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+declare namespace baseShapes {
+  export interface RECTANGLE {
+    width: number,
+    height: number,
+    x: number,
+    y: number,
+    type: 'RECTANGLE'
+  }
+}
--- src/utils/add_rectangle.ts
+++ src/utils/add_rectangle.ts
@@ -1,6 +1,6 @@
 import doc from '../client/client';
 
-function addRectangle(rectangle: any) {
+function addRectangle(rectangle: baseShapes.RECTANGLE) {
   console.log(doc);
   doc.submitOp([{ p: ['shapes', doc.data.shapes.length], li: rectangle }]);
 }
