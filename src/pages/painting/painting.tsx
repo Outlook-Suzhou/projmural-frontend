f97faaddb0c265110fcf81c8b83f8823ac475b0d
inctry <inctry@outlook.com>
2021-07-27 14:07:23 +0800

    add triangle

--- src/pages/painting/painting.tsx
+++ src/pages/painting/painting.tsx
@@ -7,16 +7,18 @@ import addRectangle from '../../utils/add_rectangle';
 import addCircle from '../../utils/add_circle';
 import addEllipse from '../../utils/add_circle';
 import addDiamond from '../../utils/add_diamond';
+import addTriangle from '../../utils/add_triangle';
 
-const Paiting: React.FC<{}> = () => {
+const Painting: React.FC<{}> = () => {
   return (
     <div className="paiting" >
       <button type="button" onClick={() => addRectangle({width: 20, height: 50, x: 30, y: 40, type: 'RECTANGLE'})}>rect</button>
       <button type="button" onClick={() => addCircle({radius: 30, x: 30, y: 40, type: 'CIRCLE'})}>circle</button>
       <button type="button" onClick={() => addEllipse({radius: {x: 20, y: 30}, x: 30, y: 40, type: 'ELLIPSE'})}>ellipse</button>
       <button type="button" onClick={() => addDiamond({radius: {x: 40, y: 25}, x: 30, y: 40, type: 'DIAMOND'})}>diamond</button>
+      <button type="button" onClick={() => addTriangle({x: 30, y: 40, points: [0, 0, 100, 0, 100, 100], type: 'TRIANGLE'})}>triangle</button>
       <PaitingContent />
     </div>
   );
 };
-export default Paiting;
+export default Painting;
