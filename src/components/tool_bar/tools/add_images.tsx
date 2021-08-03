3c0896a8b292cf9f3d9e9b3a974856f764477a62
t-liyue <181250077@smail.nju.edu.cn>
2021-08-03 17:34:22 +0800

    add uploadImage

--- src/components/tool_bar/tools/add_images.tsx
+++ src/components/tool_bar/tools/add_images.tsx
@@ -4,18 +4,38 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 // eslint-disable-next-line no-unused-vars
 import { Icon } from '@fluentui/react/lib/Icon';
+import { Upload } from 'element-react';
 import addImg from '../../../utils/add_img';
+import 'element-theme-default';
+// eslint-disable-next-line import/order
+import { fileURLToPath } from 'url';
 
+const handleAvatarScucess = (res: any, file: any) => {
+  console.log(res);
+  console.log(fileURLToPath(file.raw));
+};
+const handlePreview = (file: any) => {
+  console.log(file);
+};
 const AddImage: React.FC<{}> = () => (
   // eslint-disable-next-line object-curly-newline
   <div className="tool_icon" style={{ textAlign: 'center' }}>
-    <Icon
-      iconName="PictureFill"
-      style={{ fontSize: '40px', margin: 'auto' }}
-      onClick={() => addImg({
-        width: 20, height: 50, x: 300, y: 300, type: 'IMAGE', url: 'https://konvajs.org/assets/lion.png',
-      })}
-    />
+    <Upload
+      className="upload-demo"
+      action="//jsonplaceholder.typicode.com/posts/"
+      showFileList={false}
+      onSuccess={(res, file) => handleAvatarScucess(res, file)}
+      onPreview={(file) => handlePreview(file)}
+    >
+      <Icon
+        iconName="PictureFill"
+        style={{ fontSize: '40px', margin: 'auto' }}
+        onClick={() => addImg({
+          width: 20, height: 50, x: 300, y: 300, type: 'IMAGE', url: 'https://konvajs.org/assets/lion.png',
+        })}
+        className="avatar"
+      />
+    </Upload>
   </div>
 
 );
