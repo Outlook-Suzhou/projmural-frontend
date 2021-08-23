f4689d0338cc5ebf1cd54e5099c3064572c333f8
furfio <1722941803@qq.com>
2021-08-23 13:25:09 +0800

    change login style

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -52,46 +52,55 @@ const Login: React.FC<{}> = () => {
             isAuthenticated={isAuthenticated}
           />
         </Header>
-        <Content>
-          { !graphData.empty
-            ? (
-              <div id="profile-div">
-                <p>
-                  <strong>First Name: </strong>
-                  {' '}
-                  {graphData.givenName}
-                </p>
-                <p>
-                  <strong>Last Name: </strong>
-                  {' '}
-                  {graphData.surname}
-                </p>
-                <p>
-                  <strong>Email: </strong>
-                  {' '}
-                  {graphData.userPrincipalName}
-                </p>
-                <p>
-                  <strong>Id: </strong>
-                  {' '}
-                  {graphData.id}
-                </p>
-                <button type="button" onClick={goToPainting}>
-                  Go to Dashboard
+        <Content className="content">
+          <div className="pic">
+            { !graphData.empty
+              ? (
+                <div id="profile-div">
+                  <div className="info">
+                    <p>
+                      <strong>First Name: </strong>
+                      {' '}
+                      {graphData.givenName}
+                    </p>
+                    <p>
+                      <strong>Last Name: </strong>
+                      {' '}
+                      {graphData.surname}
+                    </p>
+                    <p>
+                      <strong>Email: </strong>
+                      {' '}
+                      {graphData.userPrincipalName}
+                    </p>
+                    <p>
+                      <strong>Id: </strong>
+                      {' '}
+                      {graphData.id}
+                    </p>
+                  </div>
+                  <button className="goToDash" type="button" onClick={goToPainting}>
+                    Go to Dashboard
+                  </button>
+                </div>
+              )
+              : null }
+            {
+              graphData.empty && isAuthenticated ? (
+                <button className="getInfo" type="button" onClick={() => requestProfileData()}>
+                  Request Info
                 </button>
-              </div>
-            )
-            : null }
-          {
-            graphData.empty && isAuthenticated ? (
-              <button type="button" onClick={() => requestProfileData()}>
-                Request Information
-              </button>
-            ) : null
-          }
+              ) : null
+            }
+          </div>
+          <div className="banner" />
         </Content>
-        <Footer>
-          Footer
+        <Footer className="footer">
+          <small>
+            <p>Microsoft Outlook Team</p>
+            <p>Intern Project</p>
+            <p>2021.8</p>
+          </small>
         </Footer>
       </Layout>
     </div>
