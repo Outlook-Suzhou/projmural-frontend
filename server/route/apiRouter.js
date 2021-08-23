95ad0388a5fe0d568b0d0628be8115ef44ff2ea5
ZerQAQ <1075843852@qq.com>
2021-08-23 16:49:42 +0800

    add login staff add proxy

--- server/route/apiRouter.js
+++ server/route/apiRouter.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const websocketRouter = express.Router();
 
 websocketRouter.use('/api', createProxyMiddleware({
-  target: 'wss://localhost:8081', // target host
+  target: 'http://localhost:8081/api', // target host
 }));
 
 module.exports = websocketRouter;
