fd4cacc8683654e0a37b5761cf794a0d5e692d22
inctry <inctry@outlook.com>
2021-08-23 13:26:06 +0800

    add

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,18 +1,12 @@
 import ReconnectingWebSocket from 'reconnecting-websocket';
-// import { Stage, Layer, Rect, Text } from 'react-konva';
-// import Konva from "konva";
 
 const sharedb = require('sharedb/lib/client');
 
-console.log(window.location.href);
-
-// Open WebSocket connection to ShareDB server
 const ipAddress = process.env.NODE_ENV === 'production' ? 'wss://www.projmural.com/api/websocket' : 'ws://localhost:8080';
-console.log(process.env.NODE_ENV);
+
 const socket = new ReconnectingWebSocket(`${ipAddress}`);
 const connection = new sharedb.Connection(socket);
 const docID = window.location.pathname.substring(10);
-
 const doc = connection.get('projmural', docID);
 doc.subscribe();
 
--- src/pages/painting/painting.tsx
+++ src/pages/painting/painting.tsx
@@ -1,10 +1,29 @@
-/* eslint-disable */
-import React, {useState} from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
 import PaitingContent from '../../components/painting_content/painting_content';
+import doc from '../../client/client';
+import axios from '../../utils/axios';
 
 const Painting: React.FC<{}> = () => {
+  const history = useHistory();
+  useEffect(() => {
+    console.log(doc);
+    axios.post('/api/doc', {
+      type: 'get',
+      data: {
+        microsoft_id: 'test',
+        canvas_id: doc.id,
+      },
+    }).then((res) => {
+      if (res.data.msg === 'ok') {
+        if (res.data.data.canvas_exist === false) {
+          history.push('/');
+        }
+      }
+    });
+  }, []);
   return (
-    <div className="paiting" >
+    <div className="paiting">
       <PaitingContent />
     </div>
   );
