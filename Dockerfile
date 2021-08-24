e7f91a2b429dc76002ff262c47553b80d5f3402e
ZerQAQ <1075843852@qq.com>
2021-08-24 15:40:42 +0800

    fix dockerfile and workflow

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,4 @@
 FROM node:14.17
 COPY . ./projmural
 WORKDIR /projmural
-RUN apt-get update
-RUN apt-get install git
-RUN npm install
-RUN npm install pm2 -g
-RUN npm run build
-ENV PM2_PUBLIC_KEY a4v373c20sxo6g3
-ENV PM2_SECRET_KEY vfl0e8fbnfwrud2
 CMD ["pm2-runtime", "./server/server.js"]
--- server/server.js
+++ server/server.js
@@ -1,38 +1,26 @@
-const https = require('https');
 const http = require('http');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const credentials = require('./credentials.js');
 const docRouter = require('./route/docRouter.js');
-const websocketRouter = require('./route/websocketRouter.js');
 const viewRouter = require('./views/view.js');
-const userRouter = require('./route/userRouter.js');
 
 require('./sharedb/sharedb.js');
 
 const isProduction = process.env.NODE_ENV !== 'development';
-const createHTTPServer = (isHTTPS) => {
+const createHTTPServer = () => {
   const app = express();
-  app.use('/api', [websocketRouter, userRouter]);
-  console.log(isHTTPS);
-  app.use(express.static(path.join(__dirname, '../build')));
   app.use(cors());
-  app.use(express.json()); // for parsing application/json
+  app.use(express.static(path.join(__dirname, '../build'))); // for parsing application/json
   app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
   app.use('/api', docRouter);
   app.use('/', viewRouter);
-  const httpsServer = isHTTPS ? https.createServer(credentials, app) : http.createServer(app);
-  if (isHTTPS) {
-    httpsServer.listen(443, () => {
-      console.log('HTTPS Server running on port 443');
-    });
-  } else {
-    httpsServer.listen(8000, () => {
-      console.log('HTTP Server running on port 8000');
-    });
-  }
+  const httpsServer = http.createServer(app);
+  httpsServer.listen(8000, () => {
+    console.log('HTTP Server running on port 8000');
+  });
 };
+
 createHTTPServer(isProduction);
