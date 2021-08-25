08e2f872bc64fbe7de76fd41d9731cbf741b2517
ZerQAQ <1075843852@qq.com>
2021-08-25 14:37:10 +0800

    shorten paintingID

--- server/sharedb/doc.js
+++ server/sharedb/doc.js
@@ -1,11 +1,13 @@
 const ShareDB = require('sharedb');
+const crypto = require('crypto');
 const { v4: uuidv4 } = require('uuid');
 
 const backend = new ShareDB();
 
-const createPaintingID = () => (
-  Buffer.from(uuidv4()).toString('base64')
-);
+const createPaintingID = () => {
+  const md5 = crypto.createHash('md5');
+  return md5.update(Buffer.from(uuidv4()).toString('base64')).digest('hex').slice(-8);
+};
 
 const createDoc = () => {
   const connection = backend.connect();
