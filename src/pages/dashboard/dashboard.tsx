931dbc95432f3247c1ad503e4d101657e5659860
ZerQAQ <1075843852@qq.com>
2021-08-25 15:19:54 +0800

    add avator and name fix client.ts

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -3,11 +3,13 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Icon } from '@fluentui/react/lib/Icon';
 import {
-  InputNumber, Modal,
+  InputNumber, Modal, PageHeader, Avatar,
 } from 'antd';
 import axios from '../../utils/axios';
+import { useStateStore } from '../../store/store';
 
 const Dashboard: React.FC<{}> = () => {
+  const state = useStateStore();
   const history = useHistory();
   const [modalVisible, setModalVisible] = useState(false);
   const [kanban, setKanban] = useState({ teamNum: 3, dateNum: 10, unit: 'day' });
@@ -45,14 +47,26 @@ const Dashboard: React.FC<{}> = () => {
   const goToKanban = () => {
     setModalVisible(true);
   };
+  const ColorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
   return (
     <>
       <div className="dashboard">
-        <div className="header">
-          <div className="avatar">
-            <Icon iconName="UserFollowed" />
-          </div>
-        </div>
+        <PageHeader
+          className="site-page-header"
+          title="DashBoard"
+          extra={
+            [
+              <div>
+                <span className="avatar_name">
+                  {state.userInfo.name}
+                </span>
+                <Avatar size={50} style={{ backgroundColor: ColorList[0], verticalAlign: 'middle' }}>
+                  {state.userInfo.name.split(' ').pop()}
+                </Avatar>
+              </div>,
+            ]
+          }
+        />
         <div className="body">
           <div className="menu">
             <div className="choose_template" />
