920735d2c6e5009772ddee87af65d61f438fabe3
ZerQAQ <1075843852@qq.com>
2021-08-26 16:19:39 +0800

    add mongodb

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,7 +7,7 @@ const ipAddress = 'wss://www.projmural.com/api/websocket';
 const socket = new ReconnectingWebSocket(`${ipAddress}`);
 const connection = new sharedb.Connection(socket);
 const docID = window.location.pathname.substring(10);
-const doc = connection.get('projmural', docID);
+const doc = connection.get('canvas', docID);
 doc.subscribe();
 
 export default doc;
