550dd26fb1b6afd34ccc6c1967a67f2d8e84442c
ZerQAQ <1075843852@qq.com>
2021-08-26 15:08:22 +0800

    add visit go api

--- server/route/docRouter.js
+++ server/route/docRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const axios = require('axios');
 const { createDoc, getDoc } = require('../sharedb/doc.js');
 
+const adminKey = 'admin_key@&(Hcnc293)C8329h*(#(*C..1-r3k,';
+const goHostName = 'http://localhost:8081';
+const postConfig = {
+  headers: {
+    Authorization: adminKey,
+  },
+};
 const docRouter = express.Router();
 const errRsp = {
   msg: 'err',
   retc: -1,
+};
+const errUserNotExistRsp = {
+  msg: 'user not exist',
+  retc: -4,
 };
 
 docRouter.post('/doc', async (req, res) => {
@@ -23,6 +35,46 @@ docRouter.post('/doc', async (req, res) => {
       return;
     }
     const ID = await createDoc(data.canva_name);
+    let usrRsp;
+    try {
+      usrRsp = await axios.post(`${goHostName}/api/user`, {
+        type: 'query',
+        data: {
+          microsoft_id: data.microsoft_id,
+        },
+      }, postConfig);
+    } catch (e) {
+      console.log(e);
+    }
+    if (usrRsp.data.retc !== 0) {
+      console.log('query:', usrRsp.data);
+      if (usrRsp.data.retc === -4) {
+        res.status(200).send(errUserNotExistRsp);
+      } else {
+        res.status(200).send(errRsp);
+      }
+      return;
+    }
+
+    const newUser = {
+      ...usrRsp.data.data,
+    };
+    newUser.canvas.push(ID);
+    let updateRsp;
+    try {
+      updateRsp = await axios.post(`${goHostName}/api/user`, {
+        type: 'update',
+        data: newUser,
+      }, postConfig);
+    } catch (e) {
+      console.log(e);
+    }
+    if (updateRsp.data.retc !== 0) {
+      console.log('update:', usrRsp.data);
+      res.status(200).send(errRsp);
+      return;
+    }
+
     res.status(200).send({
       msg: 'ok',
       retc: 0,
