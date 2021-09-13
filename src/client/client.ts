b1c9cc9bd80b827b7b00d2db510e7b5bb0ed8b35
inctry <inctry@outlook.com>
2021-09-13 13:21:25 +0800

    fix bug

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 const sharedb = require('sharedb/lib/client');
@@ -8,9 +9,13 @@ const socket = new ReconnectingWebSocket(`${ipAddress}`);
 const connection = new sharedb.Connection(socket);
 console.log(connection);
 const doc = { value: null };
+window.onerror = () => {
+  location.reload();
+};
 function getCurrentDoc(callback?: Function) {
   const docID = window.location.pathname.substring(10);
   doc.value = connection.get('canvas', docID);
+  // eslint-disable-next-line no-restricted-globals
   if (doc.value) {
     ((doc.value) as any).subscribe(() => {
       if (callback) {
