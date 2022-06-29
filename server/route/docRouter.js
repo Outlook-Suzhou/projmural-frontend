dcfe0ad2911e8215a7fcbbd51e57e61cbea1371a
Yu Yuan <33387490+Yuan98Yu@users.noreply.github.com>
2022-06-29 18:20:12 +0800

    Add function in shareDb server: dupicate a painting content using canvas_id.

--- server/route/docRouter.js
+++ server/route/docRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const { createDoc, getDoc } = require('../sharedb/doc.js');
+const { createDoc, getDoc, duplicateDoc } = require('../sharedb/doc.js');
 
 const adminKey = 'admin_key@&(Hcnc293)C8329h*(#(*C..1-r3k,';
 const goHostName = 'http://123.115.107.83:8081';
@@ -21,17 +21,19 @@ const errUserNotExistRsp = {
 
 docRouter.post('/doc', async (req, res) => {
   const { type, data } = req.body;
-  if (type !== 'create' && type !== 'get') {
+  console.log('-----------');
+  console.log(type, data)
+  if (type !== 'create' && type !== 'get' && type !== 'duplicate') {
     res.status(200).send(errRsp);
     return;
   }
   if (data === null || data === undefined || data?.microsoft_id === undefined) {
-    res.status(200).send(errRsp);
+    res.status(200).send({...errRsp, msg: 'err data'});
     return;
   }
   if (type === 'create') {
     if (data?.canva_name === undefined) {
-      res.status(200).send(errRsp);
+      res.status(200).send({...errRsp, msg: 'err canva_name undefined'});
       return;
     }
     const ID = await createDoc(data.canva_name);
@@ -93,6 +95,71 @@ docRouter.post('/doc', async (req, res) => {
       retc: 0,
       data: {
         canvas_exist: doc.type !== null,
+      },
+    });
+  } else if (type === 'duplicate') {
+    console.log('begin duplicate')
+    if (data?.canva_name === undefined) {
+      res.status(200).send({...errRsp, msg: 'err canva_name undefined'});
+      return;
+    }
+    let ID;
+    try {
+      ID = await duplicateDoc(data.canva_name, data.old_Id);
+    } catch (e) {
+      console.log('duplicate fails');
+      console.log(e);
+    }
+    let usrRsp;
+    try {
+      usrRsp = await axios.post(`${goHostName}/api/user`, {
+        type: 'query',
+        data: {
+          microsoft_id: data.microsoft_id,
+        },
+      }, postConfig);
+    } catch (e) {
+      console.log(e);
+    }
+    if (usrRsp.data.retc !== 0) {
+      console.log('query...');
+      // console.log('query:', usrRsp.data);
+      if (usrRsp.data.retc === -4) {
+        res.status(200).send(errUserNotExistRsp);
+      } else {
+        res.status(200).send({...errRsp, msg: 'err unknown retc error'});
+      }
+      return;
+    }
+
+    const newUser = {
+      ...usrRsp.data.data,
+    };
+    newUser.canvas.unshift({
+      id: ID,
+      name: data.canva_name,
+      recent_open: 0,
+    });
+    let updateRsp;
+    try {
+      updateRsp = await axios.post(`${goHostName}/api/user`, {
+        type: 'update',
+        data: newUser,
+      }, postConfig);
+    } catch (e) {
+      console.log(e);
+    }
+    if (updateRsp.data.retc !== 0) {
+      console.log('update:', usrRsp.data);
+      res.status(200).send({...errRsp, msg: 'err update user failed'});
+      return;
+    }
+
+    res.status(200).send({
+      msg: 'ok',
+      retc: 0,
+      data: {
+        canvas_id: ID,
       },
     });
   }
